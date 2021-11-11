b322bb58f8345534fd6c0d41cf89362af063a596
Collin Clifford <collinclifford@ymail.com>
2021-11-10 20:45:43 -0800

    finished-project

--- src/observations/ObservationCreate.js
+++ src/observations/ObservationCreate.js
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useHistory } from "react-router-dom";
 import { createObservation } from "../utils/api";
+import ErrorAlert from "../layout/ErrorAlert";
 
 function ObservationCreate() {
   const history = useHistory();
@@ -11,15 +12,19 @@ function ObservationCreate() {
     sky_condition: "",
   });
 
+  const [error, setError] = useState(null);
+
   function cancelHandler() {
     history.push("/");
   }
 
   function submitHandler(event) {
     event.preventDefault();
-    createObservation(observation).then(() => {
-      history.push("/");
-    });
+    createObservation(observation)
+      .then(() => {
+        history.push("/");
+      })
+      .catch(setError);
   }
 
   function changeHandler({ target: { name, value } }) {
@@ -32,6 +37,7 @@ function ObservationCreate() {
   return (
     <main>
       <h1 className="mb-3">Create Observation</h1>
+      <ErrorAlert error={error} />
       <form onSubmit={submitHandler} className="mb-4">
         <div className="row mb-3">
           <div className="col-6 form-group">
@@ -50,7 +56,7 @@ function ObservationCreate() {
               required={true}
             />
             <small className="form-text text-muted">
-              Enter a value between -90 and 90
+              Enter a value between -90 and 90.
             </small>
           </div>
           <div className="col-6">
@@ -69,7 +75,7 @@ function ObservationCreate() {
               required={true}
             />
             <small className="form-text text-muted">
-              Enter a value between -180 and 180
+              Enter a value between -180 and 180.
             </small>
           </div>
         </div>
@@ -85,14 +91,14 @@ function ObservationCreate() {
             onChange={changeHandler}
             required={true}
           >
-            <option value="">Select a sky condition option</option>+{" "}
-            <option value="100">Cloudless</option>+{" "}
-            <option value="101">Some clouds</option>+{" "}
-            <option value="102">Cloud covered</option>+{" "}
-            <option value="103">Foggy</option>+{" "}
-            <option value="104">Raining</option>+{" "}
-            <option value="106">Snowing</option>+{" "}
-            <option value="108">Hailing</option>+{" "}
+            <option value="">Select a sky condition option</option>
+            <option value="100">Cloudless</option>
+            <option value="101">Some clouds</option>
+            <option value="102">Cloud covered</option>
+            <option value="103">Foggy</option>
+            <option value="104">Raining</option>
+            <option value="106">Snowing</option>
+            <option value="108">Hailing</option>
             <option value="109">Thunderstorms</option>
           </select>
         </div>
